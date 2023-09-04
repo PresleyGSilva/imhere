827798da648a24438f25f5ae75540b0d6f4fc9cc
Presley <presleygs.1607@gmail.com>
2023-09-04 15:19:47 -0300

    Background-Creating new project

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,36 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+
+import { styles } from "./styles";
+
+export default function Home() {
+  
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Setembro de 2023.
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
